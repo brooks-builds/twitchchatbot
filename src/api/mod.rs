0369e6b89b1c9eea12c8f8da2e1ef2412c9238d4
Brooks Patton <brooks@brookzerker.com>
2020-03-28 11:24:12 -0600

    Adding in security and postman integration

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -1,12 +1,7 @@
 use super::database::command;
 use warp::Filter;
 
-pub async fn run(_api_key: String) {
-    // TODO - research how to check headers for all routes
-    // let authorize = warp::any().map(|| {
-    //     let auth_header = warp::header::optional::<String>("api_key");
-    //     dbg!(auth_header);
-    // });
+pub async fn run(api_key: String) {
     let json_reply = warp::reply::with::header("Content-Type", "application/json");
     let get_all = warp::path!("api" / "v1" / "commands")
         .map(|| {
@@ -20,47 +15,65 @@ pub async fn run(_api_key: String) {
     let get_one = warp::path!("api" / "v1" / "commands" / i32)
         .map(|id| warp::reply::json(&command::get_one(id)))
         .with(json_reply);
+    let create_api_key = api_key.clone();
     let create = warp::post()
         .and(warp::path!("api" / "v1" / "commands"))
+        .and(warp::header::<String>("api_key"))
         .and(warp::body::content_length_limit(1024 * 64))
         .and(warp::body::json())
-        .map(|new_command: command::NewCommand| {
-            if validate_new_command(&new_command.command) {
-
-                let created_command = command::insert(new_command);
-                warp::reply::with_status(
-                    warp::reply::json(&created_command),
-                    warp::http::StatusCode::CREATED,
-                )
+        .map(move |header_api_key, new_command: command::NewCommand| {
+            if &create_api_key == &header_api_key {
+                if validate_new_command(&new_command.command) {                
+                    let created_command = command::insert(new_command);
+                    warp::reply::with_status(
+                        warp::reply::json(&created_command),
+                        warp::http::StatusCode::CREATED,
+                    )
+                } else {
+                    warp::reply::with_status(warp::reply::json(&serde_json::json!({"error": "New commands must begin with ! and not have any spaces"})), warp::http::StatusCode::BAD_REQUEST)
+                }
             } else {
-                warp::reply::with_status(warp::reply::json(&serde_json::json!({"error": "New commands must begin with ! and not have any spaces"})), warp::http::StatusCode::BAD_REQUEST)
+                warp::reply::with_status(warp::reply::json(&serde_json::json!({"error": "API key missing or incorrect"})), warp::http::StatusCode::UNAUTHORIZED)
             }
-        });
-
+            });
+    let update_api_key = api_key.clone();
     let update = warp::put()
         .and(warp::path!("api" / "v1" / "commands" / i32))
         .and(warp::body::content_length_limit(1024 * 64))
         .and(warp::body::json())
-        .map(|id: i32, updated_command: command::NewCommand| {
-            if validate_new_command(&updated_command.command) {
-                let command = command::update(id, updated_command.command, updated_command.response);
-                warp::reply::with_status(warp::reply::json(&command), warp::http::StatusCode::OK)
+        .and(warp::header::<String>("api_key"))
+        .map(move |id: i32, updated_command: command::NewCommand, header_api_key| {
+            if &header_api_key == &update_api_key {
+
+                if validate_new_command(&updated_command.command) {
+                    let command = command::update(id, updated_command.command, updated_command.response);
+                    warp::reply::with_status(warp::reply::json(&command), warp::http::StatusCode::OK)
+                } else {
+                    warp::reply::with_status(warp::reply::json(&serde_json::json!({"error": "Commands must begin with ! and not have any spaces"})), warp::http::StatusCode::BAD_REQUEST)
+                }
             } else {
-                warp::reply::with_status(warp::reply::json(&serde_json::json!({"error": "Commands must begin with ! and not have any spaces"})), warp::http::StatusCode::BAD_REQUEST)
+                warp::reply::with_status(warp::reply::json(&serde_json::json!({"error": "API key missing or incorrect"})), warp::http::StatusCode::UNAUTHORIZED)
             }
         });
-
+    let destroy_api_key = api_key.clone();
     let destroy = warp::delete()
         .and(warp::path!("api" / "v1" / "commands" / i32))
-        .map(|id| match command::destroy(id) {
-            Ok(_) => warp::reply::with_status(
-                warp::reply::json(&serde_json::json!({"status": "success"})),
-                warp::http::StatusCode::OK,
-            ),
-            Err(message) => warp::reply::with_status(
-                warp::reply::json(&serde_json::json!({ "error": message })),
-                warp::http::StatusCode::INTERNAL_SERVER_ERROR,
-            ),
+        .and(warp::header::<String>("api_key"))
+        .map(move |id, header_api_key| {
+            if &destroy_api_key == &header_api_key {
+                match command::destroy(id) {
+                    Ok(_) => warp::reply::with_status(
+                        warp::reply::json(&serde_json::json!({"status": "success"})),
+                        warp::http::StatusCode::OK,
+                    ),
+                    Err(message) => warp::reply::with_status(
+                        warp::reply::json(&serde_json::json!({ "error": message })),
+                        warp::http::StatusCode::INTERNAL_SERVER_ERROR,
+                    ),
+                }
+            } else {
+                warp::reply::with_status(warp::reply::json(&serde_json::json!({"error": "API key missing or incorrect"})), warp::http::StatusCode::UNAUTHORIZED)
+            }
         });
 
     let routes = warp::get()
